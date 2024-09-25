2c397c92c4425a7b5becd379d4e4d704de5eee58
Bartłomiej Banaś <bartek.banas675@gmail.com>
2024-09-25 17:46:36 +0200

    Add DbContext class

--- Backend/BandFounder/BandFounder.Infrastructure/BandFounderDbContext.cs
+++ Backend/BandFounder/BandFounder.Infrastructure/BandFounderDbContext.cs
@@ -1,11 +1,24 @@
+using BandFounder.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace BandFounder.Infrastructure;
 
 public class BandFounderDbContext : DbContext
 {
+    public DbSet<Account> Accounts { get; set; }
+    public DbSet<SpotifyCredentials> SpotifyCredentials { get; set; }
+    
     public BandFounderDbContext(DbContextOptions options) : base(options)
     {
         
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Configuring relationships
+        modelBuilder.Entity<Account>()
+            .HasOne(account => account.SpotifyCredentials)
+            .WithOne(spotifyCredentials => spotifyCredentials.Account)
+            .HasForeignKey<SpotifyCredentials>(spotifyCredentials => spotifyCredentials.AccountId);
     }
 }
