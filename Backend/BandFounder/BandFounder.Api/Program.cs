11bd746f818cc4bbb9056594a302989635d2fc0b
Bartłomiej Banaś <bartek.banas675@gmail.com>
2024-09-25 17:47:16 +0200

    Add db context do Program.cs

--- Backend/BandFounder/BandFounder.Api/Program.cs
+++ Backend/BandFounder/BandFounder.Api/Program.cs
@@ -1,6 +1,9 @@
 using BandFounder.Api.Controllers;
+using BandFounder.Infrastructure;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
 
 // Add services to the container.
 var services = builder.Services;
@@ -9,7 +12,12 @@ services.AddControllers().AddApplicationPart(typeof(ControllerAssemblyMarker).As
 services.AddEndpointsApiExplorer();
 services.AddSwaggerGen();
 
+builder.Services.AddDbContext<BandFounderDbContext>(options =>
+    options.UseNpgsql(configuration.GetConnectionString("BandfounderDatabase")));
+
 var app = builder.Build();
+
+app.Services.CreateScope().ServiceProvider.GetRequiredService<BandFounderDbContext>().Database.EnsureCreated();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
