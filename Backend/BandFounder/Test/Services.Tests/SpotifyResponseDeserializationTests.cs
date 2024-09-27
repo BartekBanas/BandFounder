7973c025536a0584c24f8dc20a4ce6499b768a58
Bartłomiej Banaś <bartek.banas675@gmail.com>
2024-09-27 21:26:53 +0200

    Add a test for deserialization of SpotifyFollowedArtistsResponse

--- Backend/BandFounder/Test/Services.Tests/SpotifyResponseDeserializationTests.cs
+++ Backend/BandFounder/Test/Services.Tests/SpotifyResponseDeserializationTests.cs
@@ -34,4 +34,31 @@ public class SpotifyResponseDeserializationTests
         Assert.That(secondArtist.Popularity, Is.EqualTo(63));
         Assert.That(secondArtist.Genres, Does.Contain("american metalcore"));
     }
+    
+    [Test]
+    public void ShouldDeserializeSpotifyFollowedArtistsResponse()
+    {
+        // Arrange
+        var jsonFilePath = Path.Combine(TestContext.CurrentContext.TestDirectory, "spotifyFollowedArtistsResponse.json");
+
+        // Act
+        var json = File.ReadAllText(jsonFilePath);
+        var spotifyResponse = JsonSerializer.Deserialize<SpotifyFollowedArtistsResponse>(json);
+
+        // Assert
+        Assert.NotNull(spotifyResponse);
+        Assert.NotNull(spotifyResponse.Artists);
+        Assert.IsNotEmpty(spotifyResponse.Artists.Items);
+
+        var firstArtist = spotifyResponse.Artists.Items[0];
+
+        Assert.That(firstArtist.Name, Is.EqualTo("Our Last Night"));
+        Assert.That(firstArtist.Popularity, Is.EqualTo(63));
+        Assert.That(firstArtist.Id, Is.EqualTo("00YTqRClk82aMchQQpYMd5"));
+
+        var secondArtist = spotifyResponse.Artists.Items[1];
+        Assert.That(secondArtist.Name, Is.EqualTo("Slipknot"));
+        Assert.That(secondArtist.Popularity, Is.EqualTo(80));
+        Assert.That(secondArtist.Id, Is.EqualTo("05fG473iIaoy82BF1aGhL8"));
+    }
 }
