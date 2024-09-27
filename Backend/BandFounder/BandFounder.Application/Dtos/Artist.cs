41b71cfb11eb903d8075ab14b9050951b1b9bc65
Bartłomiej Banaś <bartek.banas675@gmail.com>
2024-09-27 19:33:38 +0200

    Add DTO for spotify artist

---
+++ Backend/BandFounder/BandFounder.Application/Dtos/Artist.cs
@@ -0,0 +1,14 @@
+using System.Text.Json.Serialization;
+
+namespace BandFounder.Application.Dtos;
+
+public class Artist
+{
+    [JsonPropertyName("genres")] public required List<string> Genres { get; set; }
+
+    [JsonPropertyName("id")] public required string Id { get; set; }
+
+    [JsonPropertyName("name")] public required string Name { get; set; }
+
+    [JsonPropertyName("popularity")] public int Popularity { get; set; }
+}
