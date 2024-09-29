99c8a6e87e8a1b01f49c893a3810d95b8d65cd91
Bartłomiej Banaś <bartek.banas675@gmail.com>
2024-09-29 21:53:38 +0200

    Add AccountController

---
+++ Backend/BandFounder/BandFounder.Api/Controllers/AccountController.cs
@@ -0,0 +1,96 @@
+﻿using BandFounder.Application.Dtos;
+using BandFounder.Application.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BandFounder.Api.Controllers;
+
+[ApiController]
+[Route("api/account")]
+public class AccountController : Controller
+{
+    private readonly IAccountService _accountService;
+    private readonly IUserAuthenticationService _userAuthenticationService;
+
+    public AccountController(IAccountService accountService, IUserAuthenticationService userAuthenticationService)
+    {
+        _accountService = accountService;
+        _userAuthenticationService = userAuthenticationService;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> RegisterAccount([FromBody] RegisterAccountDto dto)
+    {
+        await _accountService.RegisterAccountAsync(dto);
+
+        return Ok();
+    }
+
+    [HttpPost("authenticate")]
+    public async Task<IActionResult> Authenticate([FromBody] LoginDto dto)
+    {
+        var token = await _accountService.AuthenticateAsync(dto);
+
+        return Ok(token);
+    }
+
+    [Authorize]
+    [HttpGet]
+    public async Task<IActionResult> GetAllAccounts([FromQuery] int? pageSize, [FromQuery] int? pageNumber)
+    {
+        IEnumerable<AccountDto> accountDtos;
+
+        if (pageSize != null && pageNumber != null)
+        {
+            accountDtos = await _accountService.GetAccountsAsync((int)pageSize, (int)pageNumber);
+        }
+        else
+        {
+            accountDtos = await _accountService.GetAccountsAsync();
+        }
+
+        return Ok(accountDtos);
+    }
+
+    [Authorize]
+    [HttpGet("me")]
+    public Task<IActionResult> Me()
+    {
+        var claims = User.Claims;
+
+        var claimsInfo = claims.ToDictionary(claim => claim.Type, claim => claim.Value);
+
+        return Task.FromResult<IActionResult>(Ok(claimsInfo));
+    }
+
+    [Authorize]
+    [HttpGet("{accountGuid:guid}")]
+    public async Task<IActionResult> GetAccount([FromRoute] Guid accountGuid)
+    {
+        var user = await _accountService.GetAccountAsync(accountGuid);
+
+        return Ok(user);
+    }
+
+    [Authorize]
+    [HttpPut("me")]
+    public async Task<IActionResult> UpdateMyAccount([FromBody] UpdateAccountDto dto)
+    {
+        var userId = _userAuthenticationService.GetUserId();
+
+        var updatedAccount = await _accountService.UpdateAccountAsync(userId, dto);
+
+        return Ok(updatedAccount);
+    }
+
+    [Authorize]
+    [HttpDelete("me")]
+    public async Task<IActionResult> DeleteMyAccount()
+    {
+        var userId = _userAuthenticationService.GetUserId();
+
+        await _accountService.DeleteAccountAsync(userId);
+
+        return Ok();
+    }
+}
