cd7872492c902bbce3a9d256cfed349036295786
Bartłomiej Banaś <bartek.banas675@gmail.com>
2024-09-30 22:18:37 +0200

    Remove deprecated classes

--- Backend/BandFounder/BandFounder.Application/Dtos/Spotify/TokenStorage.cs
+++
@@ -1,8 +0,0 @@
-namespace BandFounder.Application.Dtos.Spotify;
-
-public class TokenStorage
-{
-    public required string AccessToken { get; set; }
-    public required string RefreshToken { get; set; }
-    public required DateTime ExpirationDate { get; set; }
-}
