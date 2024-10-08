f842482bb090f4649e1b550cbc758cf9d15c2712
Bartłomiej Banaś <bartek.banas675@gmail.com>
2024-10-08 23:07:35 +0200

    Add error handling middlewares

---
+++ Backend/BandFounder/BandFounder.Infrastructure/Middleware/InfrastructureErrorHandlingMiddleware.cs
@@ -0,0 +1,26 @@
+﻿using BandFounder.Infrastructure.Errors;
+using BandFounder.Infrastructure.Errors.Api;
+using Microsoft.AspNetCore.Http;
+
+namespace BandFounder.Infrastructure.Middleware;
+
+public class InfrastructureErrorHandlingMiddleware : IMiddleware
+{
+    public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+    {
+        try
+        {
+            await next.Invoke(context);
+        }
+        
+        // Infrastructure errors
+        catch (ItemNotFoundErrorException error)
+        {
+            throw new NotFoundError(error.Message, error);
+        }
+        catch (ItemDuplicatedErrorException error)
+        {
+            throw new ConflictError(error.Message, error);
+        }
+    }
+}
