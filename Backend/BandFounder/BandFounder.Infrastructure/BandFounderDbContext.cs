a9151495c6ac99d6b4d75bbd4b4feb325cb6786d
Bartłomiej Banaś <bartek.banas675@gmail.com>
2024-10-13 16:14:41 +0200

    Add relations of new entities

--- Backend/BandFounder/BandFounder.Infrastructure/BandFounderDbContext.cs
+++ Backend/BandFounder/BandFounder.Infrastructure/BandFounderDbContext.cs
@@ -11,6 +11,10 @@ public class BandFounderDbContext : DbContext
     public DbSet<Artist> Artists { get; set; }
     public DbSet<Genre> Genres { get; set; }
     
+    public DbSet<MusicianRole> MusicianRoles { get; set; }
+    public DbSet<MusicProjectListing> MusicProjectListings { get; set; }
+    public DbSet<MusicianSlot> MusicianSlots { get; set; }
+    
     public BandFounderDbContext(DbContextOptions options) : base(options)
     {
         
@@ -32,5 +36,39 @@ public class BandFounderDbContext : DbContext
         modelBuilder.Entity<Artist>()
             .HasMany(artist => artist.Genres)
             .WithMany(genre => genre.Artists);
+        
+        // Many-to-One relationship: MusicCollaboration has one owner (Account)
+        modelBuilder.Entity<MusicProjectListing>()
+            .HasOne(projectListing => projectListing.Owner)
+            .WithMany(account => account.MusicProjectListings)
+            .HasForeignKey(projectListing => projectListing.AccountId);
+
+        // Many-to-One relationship: MusicCollaboration has an optional Genre
+        modelBuilder.Entity<MusicProjectListing>()
+            .HasOne(projectListing => projectListing.Genre)
+            .WithMany()
+            .HasForeignKey(projectListing => projectListing.GenreName)
+            .IsRequired(false);
+        
+        // Configuring one-to-many relationship: MusicProjectListing has many MusicianSlots
+        modelBuilder.Entity<MusicProjectListing>()
+            .HasMany(listing => listing.MusicianSlots)
+            .WithOne(slot => slot.Listing)
+            .HasForeignKey(slot => slot.Listing.Id);
+
+        // Configuring one-to-many relationship: MusicianSlot has one MusicianRole
+        modelBuilder.Entity<MusicianSlot>()
+            .HasOne(slot => slot.Role)
+            .WithMany()
+            .HasForeignKey(slot => slot.Role.Id);
+
+        // Configuring many-to-many relationship between Account and MusicianRole
+        modelBuilder.Entity<Account>()
+            .HasMany(account => account.MusicRoles)
+            .WithMany(role => role.Accounts);
+        // .UsingEntity<Dictionary<string, object>>(
+        //     "AccountMusicRole",
+        //     j => j.HasOne<MusicianRole>().WithMany().HasForeignKey("MusicRoleId"),
+        //     j => j.HasOne<Account>().WithMany().HasForeignKey("AccountId"));
     }
 }
