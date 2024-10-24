c408d79fe9670446c24940220c99ba944465af67
Bartłomiej Banaś <bartek.banas675@gmail.com>
2024-10-24 22:47:34 +0200

    Refactor GetPagedMessagesDto and SendMessageDto

--- Backend/BandFounder/BandFounder.Application/Dtos/Messages/GetPagedMessagesDto.cs
+++ Backend/BandFounder/BandFounder.Application/Dtos/Messages/GetPagedMessagesDto.cs
@@ -1,8 +1,8 @@
 ﻿namespace BandFounder.Application.Dtos.Messages;
 
-public class GetPagedMessagesDto
+public class GetPagedMessagesDto(Guid chatRoomId, int pageSize, int pageNumber)
 {
-    public Guid ChatRoomId { get; }
-    public int PageSize { get; }
-    public int PageNumber { get; }
+    public Guid ChatRoomId { get; } = chatRoomId;
+    public int PageSize { get; } = pageSize;
+    public int PageNumber { get; } = pageNumber;
 }
--- Backend/BandFounder/BandFounder.Application/Dtos/Messages/SendMessageDto.cs
+++ Backend/BandFounder/BandFounder.Application/Dtos/Messages/SendMessageDto.cs
@@ -1,7 +1,7 @@
 ﻿namespace BandFounder.Application.Dtos.Messages;
 
-public class SendMessageDto(string content)
+public class SendMessageDto(Guid chatRoomId, string content)
 {
-    public Guid ChatRoomId { get; }
+    public Guid ChatRoomId { get; } = chatRoomId;
     public string Content { get; } = content;
 }
