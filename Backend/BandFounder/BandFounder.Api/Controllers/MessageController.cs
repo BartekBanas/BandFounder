42fdbe8e25b7de1c160cd9687201c0f099f2cf33
Bartłomiej Banaś <bartek.banas675@gmail.com>
2024-10-24 22:48:50 +0200

    Add MessageController

---
+++ Backend/BandFounder/BandFounder.Api/Controllers/MessageController.cs
@@ -0,0 +1,44 @@
+﻿using BandFounder.Application.Dtos.Messages;
+using BandFounder.Application.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BandFounder.Api.Controllers;
+
+[ApiController]
+[Route("api/chatRoom/{chatRoomId:guid}/message")]
+public class MessageController : Controller
+{
+    private readonly IMessageService _messageService;
+
+    public MessageController(IMessageService messageService)
+    {
+        _messageService = messageService;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> SendMessage([FromRoute]Guid chatRoomId, string message)
+    {
+        await _messageService.SendMessage(new SendMessageDto(chatRoomId, message));
+
+        return Ok();
+    }
+    
+    [HttpGet]
+    public async Task<IActionResult> GetChatroomMessages([FromRoute] Guid chatRoomId,
+        [FromQuery]int? pageSize, [FromQuery]int? pageNumber)
+    {
+        object messagesDto;
+
+        if (pageSize != null && pageNumber != null)
+        {
+            var request = new GetPagedMessagesDto(chatRoomId, (int)pageSize, (int)pageNumber);
+            messagesDto = await _messageService.GetChatroomPagedMessages(request);
+        }
+        else
+        {
+            messagesDto = await _messageService.GetChatroomMessages(chatRoomId);
+        }
+        
+        return Ok(messagesDto);
+    }
+}
