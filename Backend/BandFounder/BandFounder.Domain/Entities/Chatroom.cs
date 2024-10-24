30620dfb1a207603ed15e79ba869f9b2d7537f46
Bartłomiej Banaś <bartek.banas675@gmail.com>
2024-10-24 14:03:06 +0200

    Add chatroom and message entities

---
+++ Backend/BandFounder/BandFounder.Domain/Entities/Chatroom.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BandFounder.Domain.Entities;
+
+public class Chatroom : Entity
+{
+    [Key]
+    public Guid Id { get; set; }
+    
+    public required string Name { get; set; }
+
+    [ForeignKey(nameof(Owner))] public Guid? OwnerId { get; set; }
+    public virtual Account? Owner { get; set; }
+    
+    [Column(TypeName = "nvarchar(24)")]
+    public ChatRoomType ChatRoomType { get; set; }
+
+    // Many-to-Many relationship with Account
+    public virtual List<Account> Members { get; set; } = [];
+    
+    // One-to-Many relationship with Message
+    public virtual List<Message> Messages { get; set; } = [];
+}
+
+public enum ChatRoomType
+{
+    General,
+    Direct
+}
---
+++ Backend/BandFounder/BandFounder.Domain/Entities/Message.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BandFounder.Domain.Entities;
+
+public class Message : Entity
+{
+    [Key]
+    public Guid Id { get; set; }
+
+    public string Content { get; set; } = null!;
+
+    [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+    public DateTime SentDate { get; set; }
+
+    [ForeignKey(nameof(Sender))]
+    public Guid SenderId { get; set; }
+    public virtual Account Sender { get; set; } = null!;
+
+    [ForeignKey(nameof(Chatroom))]
+    public Guid ChatRoomId { get; set; }
+    public virtual Chatroom Chatroom { get; set; } = null!;
+}
