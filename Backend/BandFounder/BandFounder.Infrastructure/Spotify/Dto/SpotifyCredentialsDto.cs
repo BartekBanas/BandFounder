08effd6cbd10136475a7e1aa4deea675d186dddf
Bartłomiej Banaś <bartek.banas675@gmail.com>
2024-10-31 20:36:53 +0100

    Remove redundant annotations

--- Backend/BandFounder/BandFounder.Infrastructure/Spotify/Dto/SpotifyCredentialsDto.cs
+++ Backend/BandFounder/BandFounder.Infrastructure/Spotify/Dto/SpotifyCredentialsDto.cs
@@ -1,10 +1,8 @@
-using System.ComponentModel.DataAnnotations;
-
 namespace BandFounder.Infrastructure.Spotify.Dto;
 
 public class SpotifyCredentialsDto
 {
-    [Required] public required string AccessToken { get; set; }
-    [Required] public required string RefreshToken { get; set; }
-    [Required] public required DateTime ExpirationDate { get; set; }
+    public required string AccessToken { get; set; }
+    public required string RefreshToken { get; set; }
+    public required DateTime ExpirationDate { get; set; }
 }
