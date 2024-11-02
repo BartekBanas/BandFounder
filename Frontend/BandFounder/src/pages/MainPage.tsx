36e37fa8c0260d58ea00063bc39bb348cac31faf
Bartłomiej Banaś <bartek.banas675@gmail.com>
2024-11-02 15:47:17 +0100

    Added more "Connecting" page

--- Frontend/BandFounder/src/pages/MainPage.tsx
+++ Frontend/BandFounder/src/pages/MainPage.tsx
@@ -1,6 +1,7 @@
 import SpotifyAuthorizationButton from "../components/spotiftConnection/SpotifyAuthorizationButton";
 import React from "react";
 import {AuthorizationButton} from "../components/spotiftConnection/SpotifyConnectionButton";
+import {SpotifyDeleteCredentialButton} from "../components/spotiftConnection/spotifyDeleteCredentialButton";
 
 export function MainPage() {
     return (
@@ -8,6 +9,7 @@ export function MainPage() {
             <h1>You are logged in faggot</h1>
             <SpotifyAuthorizationButton/>
             <AuthorizationButton/>
+            <SpotifyDeleteCredentialButton/>
         </div>
     );
 }
