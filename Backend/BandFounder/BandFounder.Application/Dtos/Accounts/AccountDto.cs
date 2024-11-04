61903ad6b239a6ed5087d5bbf2f160912b7e1d17
Bartłomiej Banaś <bartek.banas675@gmail.com>
2024-11-04 21:53:27 +0100

    Remove useless attributes in AccountDto

--- Backend/BandFounder/BandFounder.Application/Dtos/Accounts/AccountDto.cs
+++ Backend/BandFounder/BandFounder.Application/Dtos/Accounts/AccountDto.cs
@@ -1,15 +1,10 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace BandFounder.Application.Dtos.Accounts;
+﻿namespace BandFounder.Application.Dtos.Accounts;
 
 public class AccountDto
 {
-    [Required] 
     public required string Id { get; init; }
-    
-    [Required]
+
     public required string Name { get; init; }
 
-    [Required]
     public required string Email { get; init; }
 }
