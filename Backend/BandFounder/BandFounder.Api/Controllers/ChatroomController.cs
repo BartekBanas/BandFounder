325e6307cfc052e39b94cbe9bba2650672424a8c
Bartłomiej Banaś <bartek.banas675@gmail.com>
2024-11-06 23:18:09 +0100

    Fix InviteToChatroom endpoint arguments and route

--- Backend/BandFounder/BandFounder.Api/Controllers/ChatroomController.cs
+++ Backend/BandFounder/BandFounder.Api/Controllers/ChatroomController.cs
@@ -44,10 +44,10 @@ public class ChatroomController : Controller
     }
 
     [Authorize]
-    [HttpPost("invite")]
-    public async Task<IActionResult> InviteToChatroom([FromBody] ChatroomInvitationDto dto)
+    [HttpPost("{chatroomId:guid}/invite/{invitedUserId:guid}")]
+    public async Task<IActionResult> InviteToChatroom(Guid chatroomId, Guid invitedUserId)
     {
-        await _chatroomService.InviteToChatroom(dto);
+        await _chatroomService.InviteToChatroom(chatroomId, invitedUserId);
 
         return Ok();
     }
