90f8666beaa13fa949a41a99c6741529472e8ae6
Bartłomiej Banaś <bartek.banas675@gmail.com>
2024-11-06 21:52:12 +0100

    Move GetMyArtists and GetUsersArtists to ContentController.cs

--- Backend/BandFounder/BandFounder.Api/Controllers/ContentController.cs
+++ Backend/BandFounder/BandFounder.Api/Controllers/ContentController.cs
@@ -1,20 +1,23 @@
 using BandFounder.Application.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BandFounder.Api.Controllers;
 
 [ApiController]
-[Route("api/content")]
+[Route("api")]
 public class ContentController : Controller
 {
     private readonly IContentService _contentService;
+    private readonly IAccountService _accountService;
 
-    public ContentController(IContentService contentService)
+    public ContentController(IContentService contentService, IAccountService accountService)
     {
         _contentService = contentService;
+        _accountService = accountService;
     }
 
-    [HttpGet("artist")]
+    [HttpGet("artists")]
     public async Task<IActionResult> GetArtists()
     {
         var artists = await _contentService.GetArtistsAsync();
@@ -22,7 +25,7 @@ public class ContentController : Controller
         return Ok(artists);
     }
 
-    [HttpGet("genre")]
+    [HttpGet("genres")]
     public async Task<IActionResult> GetGenres()
     {
         var artists = await _contentService.GetGenresAsync();
@@ -30,11 +33,31 @@ public class ContentController : Controller
         return Ok(artists);
     }
 
-    [HttpGet("role")]
+    [HttpGet("roles")]
     public async Task<IActionResult> GetMusicianRoles()
     {
         var artists = await _contentService.GetMusicianRoles();
         
+        return Ok(artists);
+    }
+    
+    [Authorize]
+    [HttpGet("account/me/artists")]
+    public async Task<IActionResult> GetMyArtists()
+    {
+        var accountDto = await _accountService.GetDetailedAccount();
+        var artists = accountDto.Artists.Select(artist => artist.Name).ToList();
+
+        return Ok(artists);
+    }
+    
+    [Authorize]
+    [HttpGet("account/{accountGuid:guid}/artists")]
+    public async Task<IActionResult> GetUsersArtists([FromRoute] Guid accountGuid)
+    {
+        var accountDto = await _accountService.GetDetailedAccount(accountGuid);
+        var artists = accountDto.Artists.Select(artist => artist.Name).ToList();
+
         return Ok(artists);
     }
 }
