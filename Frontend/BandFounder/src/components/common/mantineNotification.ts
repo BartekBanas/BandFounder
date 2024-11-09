74b03652c13f9cb0b3200b63f675e5e5f9d0b5a4
Bartłomiej Banaś <bartek.banas675@gmail.com>
2024-11-09 15:08:12 +0100

    Add dark-themed mantine notifications

---
+++ Frontend/BandFounder/src/components/common/mantineNotification.ts
@@ -0,0 +1,21 @@
+import {showNotification} from "@mantine/notifications";
+import notificationStyles from "../../styles/notificationStyles";
+import '@mantine/notifications/styles.css';
+
+export function mantineSuccessNotification(message: string): void {
+    showNotification({
+        color: 'green',
+        title: 'Success',
+        message: message,
+        styles: notificationStyles,
+    });
+}
+
+export function mantineErrorNotification(message: string): void {
+    showNotification({
+        color: 'red',
+        title: 'Error',
+        message: message,
+        styles: notificationStyles,
+    });
+}
