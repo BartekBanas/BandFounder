2a70ac07e7accb71ee4271038eaf8020cf2cc5ee
Bartłomiej Banaś <bartek.banas675@gmail.com>
2024-11-10 16:53:12 +0100

    Add const authorizedHeaders

--- Frontend/BandFounder/src/hooks/utils.ts
+++ Frontend/BandFounder/src/hooks/utils.ts
@@ -1,3 +1,10 @@
+import {getAuthToken} from "./authentication";
+
 export function sleep(ms: number) {
     return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+export const authorizedHeaders = {
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${getAuthToken()}`
 }
