f6b3e62d4b4579b2d48845f9ea378a9cf8e4c015
Bartłomiej Banaś <bartek.banas675@gmail.com>
2024-11-12 22:45:12 +0100

    Continued work on updating listings

--- Frontend/BandFounder/src/components/listing/listingOwner/api.ts
+++ Frontend/BandFounder/src/components/listing/listingOwner/api.ts
@@ -34,3 +34,62 @@ export const getListings = async () => {
         console.error('Error getting listings:', e);
     }
 }
+
+export const getGenres = async () => {
+    try{
+        const jwt = new Cookies().get('auth_token');
+        const response = await fetch(`${API_URL}/genres`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${jwt}`
+            }
+        });
+        return await response.json();
+    }
+    catch (e) {
+        console.error('Error getting genres:', e);
+    }
+}
+
+export const getMusicianRoles = async () => {
+    try{
+        const jwt = new Cookies().get('auth_token');
+        const response = await fetch(`${API_URL}/roles`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${jwt}`
+            }
+        });
+        return await response.json();
+    }
+    catch (e) {
+        console.error('Error getting musician roles:', e);
+    }
+}
+
+export const updateListing = async (listing: any, id:string) => {
+    try{
+        const jwt = new Cookies().get('auth_token');
+        if(listing.type === 'Band'){
+            listing.type = '0';
+        }
+        else {
+            listing.type = '1';
+        }
+        console.log('listing:', listing);
+        const response = await fetch(`${API_URL}/listings/${id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${jwt}`
+            },
+            body: JSON.stringify(listing)
+        });
+        return await response.json();
+    }
+    catch (e) {
+        console.error('Error updating listing:', e);
+    }
+}
