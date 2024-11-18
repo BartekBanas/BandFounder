b5e450c94cf475ffecee4e8ff081ae3195668df2
Bartłomiej Banaś <bartek.banas675@gmail.com>
2024-11-18 02:19:08 +0100

    Im so done with this. But its over. God have mercy over my soul, and let Bandfounder die in hell

--- Frontend/BandFounder/src/components/listing/listingPublic/api.ts
+++ Frontend/BandFounder/src/components/listing/listingPublic/api.ts
@@ -18,16 +18,33 @@ export const getListing = async (listingId: string) => {
     }
 }
 
-export const getListings = async () => {
+export const getListings = async (excludeOwn:boolean|undefined, matchMusicRole:boolean|undefined, sortFromLatest:boolean|undefined, showOnlyType:'CollaborativeSong'|'Band'|undefined, genre:string|undefined) => {
     try{
         const jwt = new Cookies().get('auth_token');
-        const response = await fetch(`${API_URL}/listings?ExcludeOwn=false`, {
+        let url = `${API_URL}/listings?`;
+        if(excludeOwn !== undefined){
+            url += `ExcludeOwn=${excludeOwn}&`;
+        }
+        if(matchMusicRole !== undefined){
+            url += `MatchRole=${matchMusicRole}&`;
+        }
+        if(sortFromLatest !== undefined){
+            url += `FromLatest=${sortFromLatest}&`;
+        }
+        if(showOnlyType !== undefined){
+            url += `ListingType=${showOnlyType}&`;
+        }
+        if(genre !== undefined){
+            url += `Genre=${genre}&`;
+        }
+        const response = await fetch(url, {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
                 'Authorization': `Bearer ${jwt}`
             }
         });
+        console.log(response);
         return await response.json();
     }
     catch (e) {
