6937bcd7482735123067fb2bef7cf529ebed3fe6
Bartłomiej Banaś <bartek.banas675@gmail.com>
2024-11-20 23:14:00 +0100

    Cleanup linking spotify account in the frontend app

--- Frontend/BandFounder/src/components/accountDrawer/spotifyConnection/spotifyConnection.ts
+++ Frontend/BandFounder/src/components/accountDrawer/spotifyConnection/spotifyConnection.ts
@@ -1,6 +1,4 @@
-import {SpotifyAppCredentialService} from "./spotifyAppCredentialService";
 import {API_URL} from "../../../config";
-import Cookies from "universal-cookie";
 import {
     mantineErrorNotification,
     mantineInformationNotification,
@@ -8,20 +6,15 @@ import {
 } from "../../common/mantineNotification";
 import {authorizedHeaders} from "../../../hooks/authentication";
 
-let configLoader = new SpotifyAppCredentialService();
 const BaseAppUrl = "http://localhost:3000/";
 const SpotifyConnectionPageUrl = BaseAppUrl + "spotifyConnection/callback/";
 const SpotifyAuthorizeUrl = "https://accounts.spotify.com/authorize";
 
-export type spotifyTokens = {
-    access_token: string
-    refresh_token: string
-    expires_in: number
-}
+export async function redirectToSpotifyAuthorizationPage() {
+    const spotifyClientId = await fetchSpotifyAppClientId();
 
-export function redirectToSpotifyAuthorizationPage() {
     let url = SpotifyAuthorizeUrl;
-    url += "?client_id=" + configLoader.clientId;
+    url += "?client_id=" + spotifyClientId;
     url += "&response_type=code";
     url += "&redirect_uri=" + encodeURI(SpotifyConnectionPageUrl);
     url += "&show_dialog=true";
@@ -33,7 +26,7 @@ export async function linkAccountWithSpotifyFromCode(): Promise<void> {
     const code = new URLSearchParams(window.location.search).get('code');
     if (code) {
         try {
-            await sendCodeToBackend(code);
+            await requestSpotifyAccountLinkFromCode(code);
             mantineSuccessNotification('Linking your Spotify account was successful');
         } catch (error: any) {
             mantineErrorNotification('Failed to link Spotify account');
@@ -42,7 +35,7 @@ export async function linkAccountWithSpotifyFromCode(): Promise<void> {
     }
 }
 
-async function sendCodeToBackend(code: string): Promise<void> {
+async function requestSpotifyAccountLinkFromCode(code: string): Promise<void> {
     const response = await fetch(`${API_URL}/spotifyBroker/connect`, {
         method: 'POST',
         headers: authorizedHeaders(),
@@ -61,61 +54,29 @@ async function sendCodeToBackend(code: string): Promise<void> {
     }
 }
 
-async function authorizeSpotifyAccountInBandfounder(tokens: spotifyTokens) {
-    const jwt = new Cookies().get('auth_token');
-
-    // mantineErrorNotification('Poopy');
-    // throw new Error();
-
-    const response = await fetch(`${API_URL}/spotifyBroker/connect`, {
-        method: 'POST',
+export async function fetchSpotifyAppClientId(): Promise<string> {
+    const response = await fetch(`${API_URL}/spotifyBroker/clientId`, {
+        method: 'GET',
         headers: {
             'Content-Type': 'application/json',
-            'Authorization': `Bearer ${jwt}`
+            'Access-Control-Allow-Origin': '*'
         },
-        body: JSON.stringify({
-            accessToken: tokens.access_token,
-            refreshToken: tokens.refresh_token,
-            duration: tokens.expires_in
-        })
     });
 
-    if (response.status === 409) {
-        mantineInformationNotification('Your account is already connected to a Spotify account');
-        throw new Error(await response.text());
-    } else if (!response.ok) {
-        mantineErrorNotification('An error occurred while linking your Spotify account');
-        throw new Error(await response.text());
-    }
-
-    console.log('Authorization request submitted');
-}
-
-async function linkAccountWithSpotifyArtists() {
-    const jwt = new Cookies().get('auth_token');
-    const response = await fetch(`${API_URL}/spotifyBroker/artists`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${jwt}`
-        }
-    });
+    const responseText = await response.text();
 
     if (!response.ok) {
-        mantineErrorNotification('An error occurred while linking your favourite Spotify artists');
-        throw new Error(await response.text());
+        throw new Error(responseText);
     }
+
+    return responseText;
 }
 
 export async function deleteSpotifyCredential() {
-    const jwt = new Cookies().get('auth_token');
     try {
         fetch(`${API_URL}/accounts/clearProfile`, {
             method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${jwt}`
-            }
+            headers: authorizedHeaders()
         }).then(r => {
             mantineSuccessNotification('Spotify account successfully disconnected');
         });
