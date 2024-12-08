dcf78a82429297e0dc6dd21dd8d71e5e269c908e
Bartłomiej Banaś <bartek.banas675@gmail.com>
2024-12-08 15:14:48 +0100

    Refactor applying backup

--- Backend/BandFounder/BandFounder.Application/Dtos/Backup/BackupDto.cs
+++ Backend/BandFounder/BandFounder.Application/Dtos/Backup/BackupDto.cs
@@ -1,9 +1,7 @@
-using BandFounder.Domain.Entities;
-
 namespace BandFounder.Application.Dtos.Backup;
 
 public class BackupDto
 {
     public IEnumerable<AccountBackup> Accounts { get; set; }
-    public IEnumerable<Artist> Artists { get; set; }
+    public IEnumerable<ArtistBackup> Artists { get; set; }
 }
