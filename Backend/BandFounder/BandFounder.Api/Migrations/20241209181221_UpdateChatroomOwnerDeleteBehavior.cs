401aaf38e5fd297d2cbed673b19aaf8fba67ee6c
Bartłomiej Banaś <bartek.banas675@gmail.com>
2024-12-09 20:00:29 +0100

    Add new migration

---
+++ Backend/BandFounder/BandFounder.Api/Migrations/20241209181221_UpdateChatroomOwnerDeleteBehavior.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BandFounder.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateChatroomOwnerDeleteBehavior : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Message_Account_SenderId",
+                table: "Message");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "SenderId",
+                table: "Message",
+                type: "uuid",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Message_Account_SenderId",
+                table: "Message",
+                column: "SenderId",
+                principalTable: "Account",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Message_Account_SenderId",
+                table: "Message");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "SenderId",
+                table: "Message",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uuid",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Message_Account_SenderId",
+                table: "Message",
+                column: "SenderId",
+                principalTable: "Account",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
