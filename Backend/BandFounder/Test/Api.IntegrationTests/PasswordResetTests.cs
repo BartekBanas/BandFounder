5ce9700caee9945747c921fb57951753caee824e
Bartłomiej Banaś <bartek.banas675@gmail.com>
2026-07-26 13:19:17 +0200

    Fix test 'RequestPasswordReset_KnownEmail_SendsResetLink'

--- Backend/BandFounder/Test/Api.IntegrationTests/PasswordResetTests.cs
+++ Backend/BandFounder/Test/Api.IntegrationTests/PasswordResetTests.cs
@@ -42,7 +42,7 @@ public class PasswordResetTests : IntegrationTestBase
         var email = EmailSender.Sent[0];
         Assert.That(email.To, Is.EqualTo("resetuser@example.com"));
         Assert.That(email.Subject, Does.Contain("Reset"));
-        Assert.That(email.TextBody, Does.StartWith("http://127.0.0.1:3000/reset-password?token="));
+        Assert.That(email.TextBody, Does.Contain("http://127.0.0.1:3000/reset-password?token="));
     }
 
     [Test]
